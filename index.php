256645fb53be18ee0f553c39881dd7cfca4c3c5e
GlauGLau1 <glauberjjose16@gmail.com>
2023-04-06 18:27:10 -0300

    adicionando imagem, single.php e saiba mais

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
     <h1>Primeira pagina tema wp Seja bem vindo</h1>
+    <?php
+        if(have_posts()){
+            while(have_posts()){
+                the_post();
+                the_title();
+                the_content();
+                the_excerpt();
+            }
+        }
+    ?>
+    <a href="<?php the_permalink(); ?>">Saiba Mais...</a>
 <?php get_footer(); ?>
