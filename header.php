f6e22eb538cf27df9b99aa6331115bcc34a63ee5
GlauGLau1 <glauberjjose16@gmail.com>
2023-04-06 16:27:49 -0300

    mudei de linha apenas

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Primeira Pagina</title>
         <?php wp_head(); ?>
+        <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/assets/css/bootstrap.min.css">
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
     </head>
     <body>
