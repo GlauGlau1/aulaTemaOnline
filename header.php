8ccdda19c1692c49bce151373260de7e5e4bbaaf
GlauGLau1 <glauberjjose16@gmail.com>
2023-04-06 16:04:33 -0300

    implementação do bootstrap

--- header.php
+++ header.php
@@ -6,5 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Primeira Pagina</title>
         <?php wp_head(); ?>
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
     </head>
     <body>
